3cf25b12f828f367f86b08eedd7acba0e2bdadb5
Daniel <daniel.lipowczan@gmail.com>
2020-01-26 17:13:00 +0100

    Kolokwium nr.2

--- P3_Lab10_Kol2/Program.cs
+++ P3_Lab10_Kol2/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using P3_Lab10_Kol2;
 
 namespace P3_Lab10_Kol2
 {
+
+    
     class Program
     {
+        
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Zadanie.1");
+
+            Console.WriteLine("Zadanie.2");
+
+                string naz = Console.ReadLine();
+
+                Console.WriteLine(naz.ExtRodo());
+
+            Console.WriteLine("Zadanie.3");
+
+            Console.WriteLine("Zadanie.4");
+
+            Console.WriteLine("Zadanie.5");
+
+            //var tak = Sklej<int>(10);
         }
     }
+    /*public static T Sklej<T>(T klej)
+    {
+        return klej;
+    }*/
 }
